aa60644c101c83b64a3dc9d9f44622961f5fb08d
Anass Sodor <asodor@e1r2p4.1337.ma>
2023-11-01 22:17:37 +0100

    first

---
+++ libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asodor <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 17:44:53 by asodor            #+#    #+#             */
+/*   Updated: 2023/11/01 17:44:55 by asodor           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_isalnum(int an);
+int ft_isalpha(int a);
+int ft_isascii(int a);
+int ft_isdigit(int d);
+int ft_isprint(int p);
+size_t  ft_strlen(const char *s);
+void    *ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
+
+#endif
